6922148f4a93930cc20c1d5132960c49567d3cae
qoqoqo2 <louiezdad@gmail.com>
2025-10-10 21:57:46 -0700

    add files

---
+++ stuff/minecraft/code.js
@@ -0,0 +1,3 @@
+function clicked() {
+    var game = window.open("https://turbowarp.org/869264071/embed?autoplay&fullscreen-background=%231b1b1d&turbo&offscreen&hqpen","", "fullscreen=yes", "menubar=no", "status=no", "titlebar=no");
+}
