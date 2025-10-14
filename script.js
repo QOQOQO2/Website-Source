be2b792e2e66582cf270f453caf93f870028981f
qoqoqo2 <louiezdad@gmail.com>
2025-10-13 17:25:34 -0700

    os detecter

---
+++ script.js
@@ -0,0 +1,14 @@
+function detectOS() {
+  const ua = navigator.userAgent;
+  const platform = navigator.userAgentData?.platform || navigator.platform || '';
+  if (/Windows/i.test(platform) || /Windows/i.test(ua)) return 'Windows';
+  if (/Mac/i.test(platform) || /Mac/i.test(ua)) return 'macOS';
+  if (/Android/i.test(ua)) return 'Android';
+  if (/iPhone|iPad|iPod/i.test(ua)) return 'iOS';
+  if (/Linux/i.test(platform) || /Linux/i.test(ua)) return 'Linux';
+  return 'Unknown';
+}
+
+const osHeading = document.createElement('h1');
+osHeading.textContent = "Hello " + detectOS() + " user!";
+document.body.prepend(osHeading);
